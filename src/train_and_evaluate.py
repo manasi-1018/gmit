5d2a64df36e4925df6afdd4b75c4acfffe9660b6
manasi-1018 <manasivrnkr@gmail.com>
2024-07-11 19:54:21 +0530

    Update train_and_evaluate.py

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -68,14 +68,3 @@ if __name__=="__main__":
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     train_and_evaluate(config_path=parsed_args.config)
-
-
-
-
-
-
-if __name__=="__main__":
-    args = argparse.ArgumentParser()
-    args.add_argument("--config", default="params.yaml")
-    parsed_args = args.parse_args()
-    train_and_evaluate(config_path=parsed_args.config)
